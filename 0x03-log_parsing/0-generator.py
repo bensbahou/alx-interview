d6b3a5cb4ca87aacfa13d0e2464928688b5f43d5
Younes Bensbahou <younesbensbahou@gmail.com>
2024-01-23 18:29:45 +0100

    Update 0-generator.py

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-import random
-import sys
-from time import sleep
+'''A script that generates random HTTP request logs.
+'''
+from random import randint, choice, random
+from sys import stdout
 import datetime
+from time import sleep
+
 
 for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+    sleep(random())
+    stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET {} {}\" {} {}\n".format(
+        randint(1, 255),
+        randint(1, 255),
+        randint(1, 255),
+        randint(1, 255),
         datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
+        '/projects/1216',
+        'HTTP/1.1',
+        choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        randint(1, 1024)
     ))
-    sys.stdout.flush()
+    stdout.flush()
